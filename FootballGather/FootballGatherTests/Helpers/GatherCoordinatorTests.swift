42cb418a6d9e417846dd40d3ce30768760bc4d5c
radude89 <radu.ionut.dan@gmail.com>
2022-02-09 05:30:58 +0200

    Created UT for GatherCoordinator

---
+++ FootballGather/FootballGatherTests/Helpers/GatherCoordinatorTests.swift
@@ -0,0 +1,47 @@
+//
+//  GatherCoordinatorTests.swift
+//  FootballGatherTests
+//
+//  Created by Radu Dan on 09.02.2022.
+//
+
+import XCTest
+@testable import FootballGather
+
+final class GatherCoordinatorTests: XCTestCase {
+    
+    func testStartGather_presentsGatherView() {
+        let views = makeViews()
+        let sut = GatherCoordinator(gatherEnded: .constant(false))
+        
+        sut.startGather(from: views.viewController, playersTeams: [:], animated: false)
+        
+        XCTAssertTrue(views.navController.viewControllerPushed)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeViews() -> (navController: MockNavController, viewController: UIViewController) {
+        let viewController = UIViewController()
+        viewController.loadViewIfNeeded()
+        
+        let navController = MockNavController(rootViewController: UIViewController())
+        navController.pushViewController(viewController, animated: false)
+        navController.loadViewIfNeeded()
+        navController.viewControllerPushed = false
+        
+        return (navController, viewController)
+    }
+    
+    private final class MockNavController: UINavigationController {
+        
+        var viewControllerPushed = false
+        
+        override func pushViewController(_ viewController: UIViewController, animated: Bool) {
+            super.pushViewController(viewController, animated: false)
+            viewControllerPushed = true
+        }
+        
+    }
+    
+}
